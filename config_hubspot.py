312b4956afd68fda02de221db5358b1d34ac8244
SherlonAlmeida <sherlonalmeidadasilva@gmail.com>
2023-11-06 22:21:12 -0300

    Integração da API do Mailchimp concluida

---
+++ config_hubspot.py
@@ -0,0 +1,3 @@
+"""Processa novos usuários do Mailchimp e adiciona no Hubspot"""
+def add_new_member_to_hubspot(data):
+    pass
--- config_mailchimp.py
+++ config_mailchimp.py
@@ -1,27 +1,34 @@
-"""import mailchimp_marketing as MailchimpMarketing
+from mailchimp_marketing import Client
 from mailchimp_marketing.api_client import ApiClientError
 
+#Integra com o arquivo do Hubspot
+import config_hubspot
+
+"""Apresenta os assinantes da lista desejada"""
+def show_list_members(data):
+  for member in data["members"]:
+    print(member["full_name"])
+
+
+#O programa começa a partir daqui
 try:
-  client = MailchimpMarketing.Client()
+  client = Client()
   client.set_config({
     "api_key": "1b8fa1c47faf718dc31ae1ad220f2f25-us21",
-    "server": "YOUR_SERVER_PREFIX"
+    "server": "us21"
   })
 
-  response = client.root.get_root()
-  print(response)
+  #Obtem todas as listas (Identificar o id da lista desejada)
+  #response = client.lists.get_all_lists()
+  #print(response)
+
+  #Obtem os membros assinantes da lista: https://mailchimp.com/developer/marketing/api/list-members/list-members-info/
+  response = client.lists.get_list_members_info("b7523b0850")
+  show_list_members(response)
+
+  #TO DO: criar funções de integração com o hubspot
+  config_hubspot.add_new_member_to_hubspot(response)
+
+
 except ApiClientError as error:
-  print("Error: {}".format(error.text))"""
-
-"""from mailchimp_marketing import Client
-
-mailchimp = Client()
-mailchimp.set_config({
-  "api_key": "YOUR_API_KEY",
-  "server": "YOUR_SERVER_PREFIX"
-})
-
-response = mailchimp.ping.get()
-print(response)"""
-
-import tkinter
+  print("Error: {}".format(error.text))
